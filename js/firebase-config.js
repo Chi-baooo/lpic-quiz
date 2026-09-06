ac1430b5c1625ec90fbf3929b28bcf2e58410666
rikuo chiba <rikuo1999.519@gmail.com>
2026-09-06 22:24:59 +0900

    Update firebase-config.js
    修正

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,10 +4,10 @@
 // 実際のアクセス制御は firestore.rules 側で行われます(README参照)。
 
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyB_YahMr6RRETqtssAE5CXHfc1BAUAekxU",
+  authDomain: "lpic-quiz-3b900.firebaseapp.com",
+  projectId: "lpic-quiz-3b900",
+  storageBucket: "lpic-quiz-3b900.firebasestorage.app",
+  messagingSenderId: "191172718330",
+  appId: "1:191172718330:web:29967fae0819c334bd6343",
 };
